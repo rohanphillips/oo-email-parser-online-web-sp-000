2c1801fee9a9eb4f214a184511f1ca6cd05c1d8a
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-05 17:08:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
   end
 
   def parse
-    collection = self.email_addresses_list.split(",")
+    collection = self.email_addresses_list.split(",").collect{{|n| n.trim}}
 
   end
 
